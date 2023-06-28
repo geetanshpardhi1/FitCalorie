625f6d568341fb49f0a6b3493633b8cee179f3a8
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2023-06-28 21:41:59 +0530

    created consume model for each user

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Food(models.Model):
     
@@ -12,4 +12,11 @@ class Food(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Consume(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food,on_delete=models.CASCADE)
+    
+
     
