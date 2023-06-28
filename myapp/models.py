c1499f43b4697da18000cc5b2afe7f37b73d2d6f
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2023-06-28 20:28:30 +0530

    created model for foods

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    fats = models.FloatField()
+    proteins = models.FloatField()
+    calories = models.IntegerField()
+    
+    
+    def __str__(self):
+        return self.name
+    
