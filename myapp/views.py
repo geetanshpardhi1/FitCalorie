ca088881828f09b702df2f174c10bdce2958dfcb
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2023-06-30 17:12:04 +0530

    enhancing frontend of homepage

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food,Consume
+from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
-    food = Food.objects.all()
+   
     
-    return render(request,'myapp/index.html',{'foods':food})
+    if request.method == "POST":
+        
+        foodconsume = request.POST['foodconsume']
+        consume = Food.objects.get(name=foodconsume)
+        user = request.user
+        
+        #saving data to consume model
+        c = Consume(user=user,food_consumed=consume)
+        c.save()
+        food = Food.objects.all()
+         
+    else:
+      
+        food = Food.objects.all()
+    consumed_food = Consume.objects.filter(user=request.user)
+    return render(request,'myapp/index.html',{'foods':food,'consumed_food':consumed_food})
+
